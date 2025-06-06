7f77e8ecfaded7ec084db5e2fa17e5c2a269e19f
sibeldogru <sibeldogru@gmail.com>
2025-06-06 23:34:14 +0300

    Add project files.

---
+++ Array_Pratik/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[] numbers = new int[10];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(i+1 + ". sayıyı giriniz:");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Girdiğiniz sayılar:");
+
+foreach  (int number in numbers)
+    Console.WriteLine(number);
+
+Console.WriteLine("11. sayıyı giriniz");
+int eleven = int.Parse(Console.ReadLine());
+
+int[] newNumbers = new int[numbers.Length + 1];
+for (int i = 0; i < numbers.Length; i++)
+{
+    newNumbers[i] = numbers[i];
+}
+newNumbers[newNumbers.Length - 1] = eleven;
+
+Array.Sort(newNumbers);
+Array.Reverse(newNumbers);
+
+Console.WriteLine("Dizinin büyükten küçüğe sıralanmış hali: ");
+
+foreach (int number in newNumbers)
+    Console.WriteLine(number);
+
+
+
+
